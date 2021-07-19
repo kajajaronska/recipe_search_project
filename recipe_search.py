debe6319ba1a88518547f3dc23dbac905c4bbe46
Kaja Jaronska <kaja.jaronska@gmail.com>
2021-07-19 22:40:25 +0100

    Changing home.html file to index.html file

--- recipe_search.py
+++ recipe_search.py
@@ -63,7 +63,7 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html', recipes=ingr_recipes_database)
+    return render_template('index.html', recipes=ingr_recipes_database)
 
 if __name__ == '__main__':
     app.run(debug=True)
